4c0ad2ae2a2e641532a17461198534a0fbaa6a61
SchlomoMushnik <schlomomushnik@gmail.com>
2025-11-23 08:49:20 +0100

    Rename test function and update import for clarity

--- test/test_exif.py
+++ test/test_exif.py
@@ -1,9 +1,9 @@
 
 
-def test_get_DateTimeOriginal():
-    from src.exif import get_DateTimeOriginal
+def test_get_jpg_DateTimeOriginal():
+    from src.exif import get_jpg_DateTimeOriginal  # Ensure this function is defined in src.exif
     from datetime import datetime
 
     pfad = "test/data/01d1a908976ec820cc8e89847151280bb37df94bbe_00001.jpg"
-    dt = get_DateTimeOriginal(pfad)
+    dt = get_jpg_DateTimeOriginal(pfad)
     assert dt == datetime(2020, 1, 20, 18, 31, 58)
